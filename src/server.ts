502ee8716abcde328fa4925a07dd1ffc7e524db2
Muhammad Umer Asif <142830547+MuhammadUmerAsif@users.noreply.github.com>
2024-11-11 23:28:55 +0500

    project setup

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import express from "express";
+const app = express();
+
+
+app.use(express.json());
+
+const port:number=3000
+
+app.get('/',(req,res)=>{
+    res.send('Hello World!')
+})
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`);
+});
